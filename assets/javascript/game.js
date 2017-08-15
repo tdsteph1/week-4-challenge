7dc8deadc39a6c7ea3ab76f2acff10193754a104
tdsteph1 <tdsteph1@asu.edu>
2017-08-14 17:55:58 -0700

    a

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,18 @@ var character;
 var enemiesAvailable = false;												//This allows us to know if all 3 enemies are in (Enemies Available To Attack) if that is true then 
 var enemiesInDefender = false;
 
+var buttonCounter = 0;
+
+var chosenCharacter;														//Global variable used so we know which character we chose when we enter (.button) click method
+var chosenEnemy;															//Global variable used so we know which enemy we chose when we enter (.button) click method
+
+
+//Button Area
+var yourHealth;
+var enemyHealth;
+
+var yourAttackPower;
+var EnemyCounterAttackPower;
 
 //Object1
 var Obi = 
@@ -10,6 +22,8 @@ var Obi =
 	//Attributes
 	name: "Obi-Wan Kenobi",
 	health: 125,
+	attkPower: 8,								//8 base(8, 16, 24, ...)
+	counterAttkPower: 5,
 
 	//Function1(Display Obi-Wan's Image)
 	addCharacterImage: function(emptyDiv)
@@ -24,6 +38,8 @@ var Luke =
 	//Attributes
 	name: "Luke Skywalker",
 	health: 100,
+	attkPower: 10,								//8 base(8, 16, 24, ...)
+	counterAttkPower: 5,
 
 	//Function1(Display Luke's Image)
 	addCharacterImage: function(emptyDiv)
@@ -38,6 +54,8 @@ var Sidious =
 	//Attributes
 	name: "Darth Sidious",
 	health: 150,
+	attkPower: 12,								//8 base(8, 16, 24, ...)
+	counterAttkPower: 20,
 
 	//Function1(Display Darth Sidious' Image)
 	addCharacterImage: function(emptyDiv)
@@ -52,6 +70,8 @@ var Maul =
 	//Attributes
 	name: "Darth Maul",
 	health: 180,
+	attkPower: 10,								//8 base(8, 16, 24, ...)
+	counterAttkPower: 25,
 
 	//Function1(Display Darth Maul's Image)
 	addCharacterImage: function(emptyDiv)
@@ -125,6 +145,11 @@ for(var i = 0; i < arrayOfObjects.length; i++)
 	
     character.attr("starWarsCharacterName", arrayOfObjects[i].name);	 //pass in each object's name into the attribute
     character.attr("starWarsCharacterHealth", arrayOfObjects[i].health); //pass in each object's health into the attribute
+    character.attr("starWarsCharacterAttkPower", arrayOfObjects[i].attkPower);
+    character.attr("starWarsCharacterCounterAttkPower", arrayOfObjects[i].counterAttkPower);
+
+
+   	
 	            																									
 
 	//add each character <div> with (name) (image) (health)
@@ -144,8 +169,9 @@ $(".characterContainer").on("click", function()
 		//which character image i've selected then moves chosen character(this)
 		//underneath (Your Character) located at <div id="choice">
 
-		var chosenCharacter = $(this);		  //(this) refers to the img-button that I clicked on
+		 chosenCharacter = $(this);		  //(this) refers to the img-button that I clicked on
 										  	  //store the value of the <img> so when we move the remanining characters, we don't relocate chosen character
+		
 
 		$("#choice").append(chosenCharacter); //store chosenCharacter(this) at <div id="choice">
 
@@ -200,7 +226,7 @@ $(".characterContainer").on("click", function()
 	else if(enemiesAvailable === true)
 	{
 		//Click on enemy in (Enemies Available To Attack) and move that chosen enemy to (Defender) area.
-		var chosenEnemy = $(this);						//(this) refers to the chosen enemy in the (Enemis Available To Attack)
+		 chosenEnemy = $(this);						//(this) refers to the chosen enemy in the (Enemis Available To Attack)
 
 		//Use while loop to go through 4 objects and check to see which object we are moving so we hide his name & health
 		var j = 1;
@@ -210,7 +236,7 @@ $(".characterContainer").on("click", function()
 		//store EnemyAvailableToAttack index in variable and concatenate with ("#enemyName") & ("#enemyHealth")
 		//to ensure that the right name and health get hidden when moving character to (defender) area.
 		var enemyIndex = $(this).attr("EnemiesAvailableToAttackIndex");
-		console.log(enemyIndex);
+		
 
 		//Hide Name
 		$("#enemyName" + enemyIndex).hide();
@@ -224,7 +250,7 @@ $(".characterContainer").on("click", function()
 		//We have to shift (name) & (health) left once for image3 if image2 is selected
 		if(enemyIndex === "2")
 		{
-			console.log("t");
+			
 			$("#enemyName3").css("margin-left", "280px");
 			$("#enemyHealth3").css("margin-left", "280px");
 		}
@@ -258,9 +284,88 @@ $(".characterContainer").on("click", function()
 });
 
 //Attack Enemy by clicking the Atack Button
-$(".btn").on("click", function()
+$("#attackButton").on("click", function()
+{
+	if(enemiesInDefender === false)
+	{
+		$("#errorMessage").html("No Enemy Here");
+	}
+	else
+	{
+		buttonCounter++;
+
+		$("#yourAttack").show();																		//when attack button is clicked display your damage and enemies damage message
+		$("#hisAttack").show();
+
+
+		 yourHealth = parseInt(chosenCharacter.attr("starWarsCharacterHealth"));						//your health
+		 enemyHealth = parseInt(chosenEnemy.attr("starWarsCharacterHealth"));							//enemy health
+
+		 yourAttackPower = parseInt(chosenCharacter.attr("starWarsCharacterAttkPower"));				// use this and multiply by counter for each iteration for base increase attack
+		 EnemyCounterAttackPower = parseInt(chosenEnemy.attr("starWarsCharacterCounterAttkPower")); 	// Enemies counter attack will remain the same, yours goes up for each attack.
+		
+		//This is where the fighting begins
+
+		if(yourHealth <= 0)
+		{
+			$("#loseMsg").show();
+			$("#restartButton").show();
+		}
+		else if(enemyHealth <= 0)
+		{
+			$("#winMsg").show();
+		}
+		else
+		{
+			
+			//Where chosenCharacter and enemyCharacter attack causing damage
+			enemyHealth = enemyHealth - (buttonCounter * yourAttackPower);						//you attack enemy
+
+			yourHealth = yourHealth - EnemyCounterAttackPower;
+
+			console.log(enemyHealth);
+			console.log(yourHealth);
+
+			//Display chosen character's health status in Your characterImage && display results in <h5> message
+			$("#chosenHealth").text(yourHealth);
+			$(".enemyYouAttack").text(chosenEnemy.attr("starWarsCharacterName"));
+			$("#yourAttackDamage").text(buttonCounter * yourAttackPower)
+
+			//Display enemy's health status in Defender image && display results in <h5> msg
+			$("#defenderHealth").text(enemyHealth);
+			$(".enemyYouAttack").text(chosenEnemy.attr("starWarsCharacterName"));
+			$("#enemyAttackDamage").text(EnemyCounterAttackPower)
+
+
+		}
+
+
+
+	}
+	
+
+});
+
+//Restart Everything
+$("#restartButton").on("click", function()
 {
 
-	$("#errorMessage").html("No Enemy Here");
+
+ enemiesAvailable = false;												//This allows us to know if all 3 enemies are in (Enemies Available To Attack) if that is true then 
+ enemiesInDefender = false;
+ buttonCounter = 0;
+
+
+
+//Button Area
+ yourHealth = 0;
+ enemyHealth = 0;
+
+yourAttackPower = 0;
+EnemyCounterAttackPower = 0;
+
+
+
+
 
 });
